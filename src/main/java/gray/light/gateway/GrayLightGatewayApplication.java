2468a80de717ef85e3182cc3c9a16da53f3f451e
XyParaCrim <xylome@qq.com>
2021-05-26 21:08:40 +0800

    feat: 项目初始化

---
+++ src/main/java/gray/light/gateway/GrayLightGatewayApplication.java
@@ -0,0 +1,18 @@
+package gray.light.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网管服务入口
+ *
+ * @author XyParaCrim
+ */
+@SpringBootApplication
+public class GrayLightGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrayLightGatewayApplication.class, args);
+    }
+
+}
